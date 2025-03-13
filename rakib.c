18c053893e917637f10b24184eb6850cb9ef1056
Rakibul Hasan <apww908@gmail.com>
2025-03-13 23:07:21 +0600

    Add files via upload
    #3, C programing

---
+++ rakib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+  int num;
+  char name ;
+ 
+
+
+
+  printf("enter character and number :");
+  scanf (" %c %d", &name, &num);
+
+
+
+  printf ("character : %c number : %d", name , num );
+  return 0 ;
+}
