16ccd5391753500b3e2d734b1bdc0e39dda26b60
Rakibul Hasan <apww908@gmail.com>
2025-03-14 16:23:32 +0600

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+int a=6, b=3, c ;
+
+c=a+b;
+printf ("a+b = %d\n",c);
+
+c=a-b;
+printf ("a-b = %d\n",c);
+
+c=a/b;
+printf ("a/b = %d\n",c);
+
+c=a*b;
+printf ("a*b = %d\n",c);
+
+c=a%b;
+printf ("reminder me when a divided by b = %d\n",c);
+
+return 0;
+}
